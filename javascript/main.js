318780a7423612d5e81b186391fd21c711d4da25
Tri Ton <triton@MacBook-Pro.local>
2017-04-24 23:03:29 -0700

    updates

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,9 @@
 
 
 
-var topics = ['bmw', 'mercedes', 'honda'];
+var topics = ['bmw', 'mercedes', 'audi', 'lexus'];
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=";
-var limit = "&limit=1"
+var limit = "&limit=20"
 var apiKey = "&api_key=dc6zaTOxFJmzC";
 
 
@@ -18,7 +18,6 @@ function renderGifs() {
 		a.attr("gif-buttons", topics[i]);
 		a.text(topics[i]);
 		$("#gif-btns").append(a);
-		console.log(topics[i]);
 	}
 }
 
@@ -36,20 +35,51 @@ $(".add-gif").on("click", function(event) {
 
 
 $(document).on("click", ".get-gif", function() {
+	$("#gif-container").empty();
+
 	var gif = $(this).attr("gif-buttons");
 
 	$.ajax({
 		url: queryURL+gif+apiKey+limit,
 		method: 'GET'
 	}).done(function(json) {
-		console.log(JSON.stringify(json));
+
+		for(var i = 0; i < json.data.length; i++){
+
+		var gifImage = $("<div>");
+		gifImage.addClass("col-md-4 theGif");
+
+		var rating = json.data[i].rating;
+		var r = $("<p>").text("Rated: " +rating);
+		gifImage.append(r);
+		$("#gif-container").append(gifImage);
+
+		var stillURL = json.data[i].images.fixed_height_still.url;
+		var animateURL = json.data[i].images.fixed_height.url;
+
+		var gifImg = $("<img>").attr("src", animateURL);
+		gifImg.attr("data-state", "still");
+		gifImg.attr("data-animate", animateURL);
+		gifImg.attr("data-still", stillURL);
+		gifImage.append(gifImg);
+		$("#gif-container").append(gifImage);
+
+		}
+
 	}).fail(function(err){
 		console.log(err);
 		console.log("System failed to retrieve the requested API");
 	});
 
-	// add html code here to display images
 });
+
+
+$(document).on("click", ".theGif", function() {
+	var state = $(this).attr("data-still");
+	console.log(state);
+
+})
+
 
 
 
